109e37ef85f27e603fa11f329f3bb05a614ea9aa
mhafet17 <do.crazy192@gmail.com>
2020-03-27 06:20:26 +0700

    Mematikan Fitur Register Di Routes

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Auth::routes();
+
+Route::match(['GET', 'POST'], '/register', function () {
+    return redirect('/login');
+})->name('register');
+
+Route::get('/home', 'HomeController@index')->name('home');
