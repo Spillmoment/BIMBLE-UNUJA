120db66686a14402c19f9132f51361873fc78123
mhafet17 <do.crazy192@gmail.com>
2020-04-11 06:45:38 +0700

    Menambah Main Untuk  Yield

---
+++ resources/views/web/layouts/main.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+</head>
+
+<body style="padding-top: 72px;">
+
+    @include('web.layouts.style')
+
+    @include('web.layouts.header')
+
+    @yield('content')
+
+    @include('web.layouts.footer')
+    
+    @include('web.layouts.script')
+
+
+</body>
+</html>
