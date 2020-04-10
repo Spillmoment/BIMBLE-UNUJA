a8e098300672b1f84beb2715afc8f89c8144a2ea
mhafet17 <do.crazy192@gmail.com>
2020-04-11 00:32:08 +0700

    Add Script Untuk Admin

---
+++ resources/views/admin/layouts/script.blade.php
@@ -0,0 +1,19 @@
+ <!-- General JS Scripts -->
+ <script src="{{asset('assets/backend/bootstrap/js/jquery-3.4.1.min.js')}}"></script>
+<script src="{{asset('assets/backend/bootstrap/js/popper.min.js')}}">
+</script>
+<script src="{{asset('assets/backend/bootstrap/js/bootstrap.min.js')}}">
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
+<script src="{{asset('assets/backend/js/moment.min.js') }}"></script>
+<script src="{{asset('assets/backend/js/stisla.js')}}"></script>
+
+<!-- JS Libraies -->
+
+<!-- Template JS File -->
+<script src="{{asset('assets/backend/js/scripts.js')}}"></script>
+<script src="{{asset('assets/backend/js/custom.js')}}"></script>
+
+{{-- Javascript --}}
+
+<!-- Page Specific JS File -->
