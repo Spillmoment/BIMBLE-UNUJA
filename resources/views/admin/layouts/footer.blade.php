5b2236e7496ff6fcf4f1db84a86075655ac0da84
mhafet17 <do.crazy192@gmail.com>
2020-04-11 00:31:49 +0700

    Add Footer Untuk Admin

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright &copy; 2020 <div class="bullet"></div> Code & Design by <a
+            href="https://www.github.com/">HDD</a>
+    </div>
+    <div class="footer-right">
+        Made with <span class="text-danger"> &#10084;</span> by HDD
+    </div>
+</footer>
+</div>
+</div>
