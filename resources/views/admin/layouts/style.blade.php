1645924768a9b6a9145f59bcd7cd7446336b363c
mhafet17 <do.crazy192@gmail.com>
2020-04-11 00:31:01 +0700

    Add Style Untuk Admin

---
+++ resources/views/admin/layouts/style.blade.php
@@ -0,0 +1,12 @@
+  <!-- General CSS Files -->
+  {{-- <link rel="icon" href="<?=base_url('assets/')?>img/favicon.png" type="image/png"> --}}
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500;700;900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Poppins:500,600,700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="{{asset('assets/backend/bootstrap/css/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/backend/fontawesome-free-5/css/all.min.css')}}">
+  
+  <!-- CSS Libraries -->
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="{{asset('assets/backend/css/style.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/backend/css/components.css')}}">
