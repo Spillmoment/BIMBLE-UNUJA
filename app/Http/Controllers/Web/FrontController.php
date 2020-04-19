e8b5c3b261c3a79079a8843637dea534e81ada7d
mhafet17 <do.crazy192@gmail.com>
2020-04-19 19:40:12 +0700

    Menggabungkan form pencarian dan filter kategori

--- app/Http/Controllers/Web/FrontController.php
+++ app/Http/Controllers/Web/FrontController.php
@@ -14,23 +14,25 @@ class FrontController extends Controller
         $kursus = Kursus::orderBy('created_at', 'DESC')->paginate(4);
         $kategori = Kategori::latest()->get();
 
-
         $keyword = $request->get('keyword');
         $filter_kategori = $request->get('nama_kategori');
         $nama_kategori = '';
 
-        if ($keyword) {
-            $kursus = Kursus::where('nama_kursus', 'LIKE', "%$keyword%")
-                ->paginate(4);
-        }
 
-        if ($filter_kategori) {
-            $kursus = Kursus::where('id_kategori', $filter_kategori)
+        if ($filter_kategori != '' || $keyword != '') {
+
+            $kursus = Kursus::with('kategori')
+                ->whereHas('kategori', function ($query) use ($keyword) {
+                    $query->where('nama_kursus', 'LIKE', "%$keyword%");
+                })
+                ->where('id_kategori', 'LIKE', "%$filter_kategori%")
                 ->orderBy('created_at', 'DESC')
                 ->paginate(4);
 
             $data_kategori = Kategori::findOrFail($filter_kategori);
             $nama_kategori = $data_kategori->nama_kategori;
+        } else {
+            $kursus = Kursus::orderBy('created_at', 'DESC')->paginate(4);
         }
 
         return view('web.web_home', compact('kursus', 'kategori', 'nama_kategori'));
