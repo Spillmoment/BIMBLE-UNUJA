1118d726bf89cc42934db2db2892adbda2220ffc
mhafet17 <do.crazy192@gmail.com>
2020-04-19 19:41:11 +0700

    Modifikasi Form Request Kursus

--- app/Http/Requests/KursusRequest.php
+++ app/Http/Requests/KursusRequest.php
@@ -26,7 +26,7 @@ class KursusRequest extends FormRequest
         return [
             'nama_kursus'   => 'required|min:3|max:100',
             'id_kategori'   => 'required',
-            'id_tutor'   => 'required',
+            'id_tutor'      => 'required',
             'gambar_kursus' => 'required|nullable|image|mimes:jpeg,jpg,png,bmp',
             'biaya_kursus'  => 'required|digits_between:0,10',
             'diskon_kursus' => 'required|digits_between:0,10',
