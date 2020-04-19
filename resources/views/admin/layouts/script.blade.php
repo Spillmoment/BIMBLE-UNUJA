b0663d6155927bc9167b156132fc1b0146bbe2d5
mhafet17 <do.crazy192@gmail.com>
2020-04-19 19:47:49 +0700

    Menambahkan Library Nicescroll

--- resources/views/admin/layouts/script.blade.php
+++ resources/views/admin/layouts/script.blade.php
@@ -4,7 +4,8 @@
 </script>
 <script src="{{asset('assets/backend/bootstrap/js/bootstrap.min.js')}}">
 </script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
+
+<script src="{{asset('assets/backend/jquery.nicescroll-3.7.6/jquery.nicescroll.min.js') }}"></script>
 <script src="{{asset('assets/backend/js/moment.min.js') }}"></script>
 <script src="{{asset('assets/backend/js/stisla.js')}}"></script>
 
