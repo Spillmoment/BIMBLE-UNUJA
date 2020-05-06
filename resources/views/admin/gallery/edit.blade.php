9235ebd5efb1972e3072bb32fb7f9b4066a494d2
mhafet17 <do.crazy192@gmail.com>
2020-05-06 23:38:54 +0700

    add views gallery

---
+++ resources/views/admin/gallery/edit.blade.php
@@ -0,0 +1,60 @@
+@extends('admin.layouts.main')
+
+@section('title','Admin - Ubah Data Gallery')
+
+@section('content')
+
+  <div class="main-content">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2">
+
+                    <div class="card card-primary">
+                        <div class="col-md-12 text-center">
+                            <p class="registration-title font-weight-bold display-4 mt-4" style="font-size: 50px;">
+                                Ubah Gallery</p>
+                            <p style="line-height:-30px;margin-top:-20px;">Silahkan ubah data data yang diperlukan
+                                dibawah </p>
+                            <hr>
+                        </div>
+
+                        <div class="card-body">
+                                <div class="col-md-12 bg-white" style="border-radius:3px;box-shadow:rgba(0, 0, 0, 0.03) 0px 4px 8px 0px">
+                                    <form method="post" action="{{route('gallery.update',$gallery->id)}}" enctype="multipart/form-data">
+                                      @csrf
+                                     @method('PUT')
+
+                                       <div class="form-group">
+                                         <label for="kursus_id">Kursus</label>
+                                         <select class="form-control" name="kursus_id" id="kursus_id">
+                                         <option value="{{ $gallery->kursus_id }}"> Jangan Diubah</option>
+                                         
+                                           @foreach ($kursus as $kursus)      
+                                         <option value="{{ $kursus->id }}">
+                                          {{ $kursus->nama_kursus }}
+                                        </option> 
+                                           @endforeach
+                                          
+                                         </select>
+                                       </div>
+                                        
+                                    <div class="form-group">
+                                      <label for="image">Image</label>
+                                      <input type="file" class="form-control-file" name="image" id="imahe" placeholder="" aria-describedby="fileHelpId">
+                                      </div>
+                                        
+                                       
+                                        <button type="submit" class="btn btn-block btn-success">
+                                          <big>Edit  Gallery</big></button>
+                                </div>
+
+
+                            </form>
+                        </div>
+                    </div>
+                 
+                </div>
+            </div>
+        </div>
+  </div>
+@endsection
---
+++ resources/views/admin/gallery/index.blade.php
@@ -0,0 +1,86 @@
+@extends('admin.layouts.main')
+
+@section('title','Admin - Data Kursus')
+
+@section('content')
+    
+<div class="main-content">
+    <section class="section">
+        <div class="section-header">
+            <h1>Management Data Gallery </h1>
+        </div>
+
+        <nav class="breadcrumb ml-4" style="margin-top: -20px">
+            <a class="breadcrumb-item" href="{{ route('manager.home') }}">Home</a>
+        <a class="breadcrumb-item active" href="{{ route('gallery.index') }}">Gallery</a>
+        </nav>
+                 
+        @if(session('status'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>{{session('status')}}</strong> 
+            <button type="button" class="close text-light" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+         @endif
+
+        <div class="row" style="overflow: scroll">
+            <div class="col-md-12">
+                <div class="container bg-white p-4"
+                    style="bgallery-radius:3px;box-shadow:rgba(0, 0, 0, 0.03) 0px 4px 8px 0px">
+                    
+                    <a href="{{ route('gallery.create') }}" class="btn btn-success my-1 float-right">
+                      <i class="fa fa-plus"></i> Tambah Gallery</a>
+                     
+                    <table class="table align-items-center table-flush">
+                        <thead class="thead-light">
+                            <tr>
+                                <th>ID</th>
+                                <th>Kursus</th>
+                                <th>Gambar</th>
+                                <th>Option</th>
+                               
+                            </tr>
+                        </thead>
+                   
+                        <tbody>
+                            @foreach ($gallery as $gallery)
+                            <tr>
+                              <td> {{$gallery->id}} </td>
+                                <td> {{ $gallery->kursus->nama_kursus }} </td>
+                              <td>
+                              <img src="{{ Storage::url($gallery->image) }}" alt="" srcset="" width="150px" class="img-thumbnail">
+                              </td>
+                              
+                                <td>
+                                    <a class="badge badge-warning text-white badge-pill" href="{{route('gallery.edit',
+                                       [$gallery->id])}}"> <i class="fa fa-edit"></i> Edit</a>
+                            <form onsubmit="return confirm('yakin untuk memasukkan ke Trash!')" class="d-inline" action="{{route('gallery.destroy', [$gallery->id])}}"   method="POST">
+                                @method('DELETE')
+                                @csrf
+                                <button type="submit" value="Delete" class="badge badge-danger badge-pill">
+                                    <i class="fa fa-trash"></i> Delete
+                                </button>
+                            </form>
+                        </td>
+                        
+                    </tr>           
+                    @endforeach
+                    
+                        </tbody>
+              
+                    </table>
+
+                    <div class="text-center">
+
+        
+                    </div>
+                        
+                </div>
+            </div>
+        </div>
+</div>
+</div>
+</div>
+
+@endsection
