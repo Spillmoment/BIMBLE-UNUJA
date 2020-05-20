eca0026f565343a23ef7f33aecd96673bcc0c6ae
gunawan <deddygunawan98@gmail.com>
2020-05-21 06:09:43 +0700

    nambah route api outlet

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['as' => 'api.', 'namespace' => 'Api'], function () {
+    /*
+     * Outlets Endpoints
+     */
+    Route::get('outlets', 'OutletController@index')->name('outlets.index');
+});
