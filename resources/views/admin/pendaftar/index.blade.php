671d72dcd1b8b050b992a6014570eaa87dcdc044
mhafet17 <do.crazy192@gmail.com>
2020-06-03 23:22:48 +0700

    Integrasi views pendaftar

--- resources/views/admin/pendaftar/index.blade.php
+++ resources/views/admin/pendaftar/index.blade.php
@@ -1,82 +1,51 @@
-@extends('admin.layouts.main')
+@extends('admin.layouts.default')
 
-@section('title','Admin - Data Pendaftar')
-
+@section('title','Bimble - Data Pendaftar')
 @section('content')
-    
-<div class="main-content">
-    <section class="section">
-        <div class="section-header">
-            <h1>Management Data Pendaftar </h1>
-        </div>
+    <div class="orders">
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+            <div class="card-body">
+              <h4 class="box-title">List Pendaftar</h4>
 
-        <nav class="breadcrumb ml-4" style="margin-top: -20px">
-            <a class="breadcrumb-item" href="{{ route('manager.home') }}">Home</a>
-        <a class="breadcrumb-item active" href="{{ route('pendaftar.index') }}">Pendaftar</a>
-        </nav>
-                 
-        @if(session('status'))
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            <strong>{{session('status')}}</strong> 
-            <button type="button" class="close text-light" data-dismiss="alert" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-         @endif
-             
-         <form action="{{route('pendaftar.index')}}">                
-          <div class="row">
+              @if(session('status'))
+              @push('after-script')
+              <script>
+                  swal({
+                  title: "Success",
+                  text: "{{session('status')}}",
+                  icon: "success",
+                  button: false,
+                  timer: 2000
+                  });
+              </script>
+              @endpush
+              @endif
 
-              <div class="col-md-5 ml-3">
-                  <div class="form-group">
-                      <input type="text" class="form-control" {{ Request::get('keyword') }} name="keyword"  placeholder="Masukkan Nama Pendaftar">
-                  </div>
-              </div>
+              <form action="{{route('pendaftar.index')}}">    
 
-              <div class="col-md-3">
-                  <div class="form-check" style="font-size: 17px">
+                <div class="row mt-2">
 
-                      <label class="form-check-label" for="active">Aktif  </label>
-                      <span class="ml-4">
-                          <input {{ Request::get('status') == 'ACTIVE' ? 'checked' : ''}} value="ACTIVE" name="status" type="radio" class="form-check-input mt-2" id="active">
-                      </span>
-                          
-                      <label class="form-check-label" for="inactive">Nonaktif </label>
-                          <span class="ml-4">
-                          <input {{Request::get('status') == 'INACTIVE' ? 'checked' : ''}} value="INACTIVE" name="status" type="radio" class="form-check-input mt-2" id="inactive">
-                      </span>
-
-                      <input type="submit" value="Filter" class="btn btn-primary">
-                  </div>
-              </div>
-
-              <hr>
-              <div class="col-md-3 ">
-                  <ul class="nav nav-pills card-header-pills">
-                      <li class="nav-item">
-                      <a class="nav-link active" href="
-                      {{route('pendaftar.index')}}">Published</a>
-                      </li>
-                      <li class="nav-item">
-                      <a class="nav-link" href="
-                      {{route('pendaftar.trash')}}">Trash</a>
-                      </li>
-                      </ul>
-              </div>
-
-
-          </div>
-          </form>
-
-
-        <div class="row" style="overflow: scroll">
-            <div class="col-md-12">
-                <div class="container bg-white p-4"
-                    style="border-radius:3px;box-shadow:rgba(0, 0, 0, 0.03) 0px 4px 8px 0px">
-                   
-
-                    <table class="table align-items-center table-flush">
-                        <thead class="thead-light">
+                    <div class="col-md-5 col-xs-5">
+                        <div class="form-group">
+                            <input type="text" class="form-control" {{ Request::get('keyword') }} name="keyword"  placeholder="Masukkan Nama" autofocus>
+                        </div>
+                    </div>
+                            <div class="col-md-3">
+                                <button type="submit" class="btn btn-secondary"> <i class="fa fa-search" aria-hidden="true"></i> </button>
+                                <a href="{{ route('pendaftar.index') }}" class="btn btn-warning text-light ml-2"> <i class="fa fa-refresh" aria-hidden="true"></i> </a>
+                            </div>
+                           
+                </div>
+              </form>
+            </div>
+            <div class="card-body--">
+              
+        
+              <div class="table-stats order-table ov-h">
+                <table class="table table-hover">
+                  <thead>
                             <tr>
                                 <th>No</th>
                                 <th>Nama Pendaftar</th>
@@ -101,13 +70,17 @@
                                 <td>Perempuan</td>
                             @endif
                             <td>{{ $regis->alamat }}</td>
-                                @if($regis->foto)
-                                <td> <img src="{{ asset('storage/'.$regis->foto) }}" width="50px"> </td>
-                                @else
-                                Tidak Ada Gambar
-                                @endif
+                               
+                            <td>
+                                @if($regis->foto) 
+                                    <img src="{{ Storage::url('uploads/pendaftar/profile/'.$regis->foto) }}" width="50px"> 
+                                    @else
+                                    N/A
+                                    @endif
+                                </td>   
+                                
                                 <td>
-                                  @if($regis->status == "ACTIVE")
+                                  @if($regis->status == 1)
                                   <span class="badge badge-success">
                                   Aktif
                                   </span>
@@ -118,16 +91,14 @@
                                   @endif
                                   </td>
                                 <td>
-                                    <a class="badge badge-info text-white badge-pill" href="{{route('pendaftar.edit',
-                                       [$regis->id])}}"> <i class="fa fa-edit"></i> Edit</a>
-                                    <a class="badge badge-warning text-white badge-pill" href="{{route('pendaftar.show',
-                                       [$regis->id])}}"> <i class="fa fa-eye"></i> Detail</a>
+                                    <a class="btn btn-info text-white btn-sm" href="{{route('pendaftar.show',
+                                       [$regis->id])}}"> <i class="fa fa-eye"></i></a>
                                  
                             <form onsubmit="return confirm('yakin untuk memasukkan ke Trash!')" class="d-inline" action="{{route('pendaftar.destroy', [$regis->id])}}"   method="POST">
                                 @method('DELETE')
                                 @csrf
-                                <button type="submit" value="Delete" class="badge badge-danger badge-pill">
-                                    <i class="fa fa-trash"></i> Trash
+                                <button type="submit" value="Delete" class="btn btn-danger btn-sm">
+                                    <i class="fa fa-trash"></i> 
                                 </button>
                             </form>
                         </td>
---
+++ resources/views/admin/pendaftar/show.blade.php
@@ -0,0 +1,35 @@
+@extends('admin.layouts.default')
+
+@section('title','Bimble - Data Pendaftar')
+@section('content')
+    <div class="orders">
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+            <div class="card-body">
+              <h4 class="box-title">Detail Pendaftar</h4>
+
+            
+
+            <div class="card-body--">
+              
+             
+              <div class="table-stats order-table ov-h">
+                <table class="table table-hover">
+             <h1>Proses ...</h1>
+              
+                    </table>
+
+                    <div class="text-center">
+
+        
+                    </div>
+                        
+                </div>
+            </div>
+        </div>
+</div>
+</div>
+</div>
+
+@endsection
