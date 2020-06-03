c2fbd3e3e0e77d83c6eb972614c9b32e3f3ad7b8
mhafet17 <do.crazy192@gmail.com>
2020-06-03 23:17:59 +0700

    Integrasi views index kategori

--- resources/views/admin/kategori/index.blade.php
+++ resources/views/admin/kategori/index.blade.php
@@ -1,150 +1,95 @@
-@extends('admin.layouts.main')
+@extends('admin.layouts.default')
 
-@section('title','Admin - Data Kategori')
-
+@section('title','Bimble - Data Kategori')
 @section('content')
-    
-<div class="main-content">
-    <section class="section">
-        <div class="section-header">
-            <h1>Management Data Kategori Kursus</h1>
-        </div>
+    <div class="orders">
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+            <div class="card-body">
+              <h4 class="box-title">List Kategori</h4>
 
-        <nav class="breadcrumb ml-4" style="margin-top: -20px">
-            <a class="breadcrumb-item" href="{{ route('manager.home') }}">Home</a>
-        <a class="breadcrumb-item active" href="{{ route('kategori.index') }}">Kategori</a>
-        </nav>
+              @if(session('status'))
+              @push('after-script')
+              <script>
+                  swal({
+                  title: "Success",
+                  text: "{{session('status')}}",
+                  icon: "success",
+                  button: false,
+                  timer: 2000
+                  });
+              </script>
+              @endpush
+              @endif
 
-    
-                
-        @if(session('status'))
-        @push('scripts')
-        <script>
-            swal({
-            title: "Success",
-            text: "{{session('status')}}",
-            icon: "success",
-            button: false,
-            timer: 1500
-            });
-        </script>
-        @endpush
-         @endif
+              <form action="{{route('kategori.index')}}">    
 
-      
-            <form action="{{route('kategori.index')}}">                
-                <div class="row">
+                <div class="row mt-2">
 
-                    <div class="col-md-5 ml-3">
+                    <div class="col-md-5">
                         <div class="form-group">
-                            <input type="text" class="form-control" {{ Request::get('keyword') }} name="keyword"  placeholder="Masukkan Nama Kategori">
+                            <input type="text" class="form-control" {{ Request::get('keyword') }} name="keyword"  placeholder="Masukkan Nama Kategori" autofocus>
                         </div>
                     </div>
-
-                    <div class="col-md-3">
-                        <div class="form-check" style="font-size: 17px">
-
-                            <label class="form-check-label" for="active">Aktif  </label>
-                            <span class="ml-4">
-                                <input {{ Request::get('status') == 'ACTIVE' ? 'checked' : ''}} value="ACTIVE" name="status" type="radio" class="form-check-input mt-2" id="active">
-                            </span>
-                                
-                            <label class="form-check-label" for="inactive">Nonaktif </label>
-                                <span class="ml-4">
-                                <input {{Request::get('status') == 'INACTIVE' ? 'checked' : ''}} value="INACTIVE" name="status" type="radio" class="form-check-input mt-2" id="inactive">
-                            </span>
-
-                            <input type="submit" value="Filter" class="btn btn-primary">
-                        </div>
-                    </div>
-
-                    <hr>
-                    <div class="col-md-3 ">
-                        <ul class="nav nav-pills card-header-pills">
-                            <li class="nav-item">
-                            <a class="nav-link active" href="
-                            {{route('kategori.index')}}">Published</a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="
-                            {{route('kategori.trash')}}">Trash</a>
-                            </li>
-                            </ul>
-                    </div>
-
-
+                            <div class="col-md-3">
+                                <button type="submit" class="btn btn-secondary"> <i class="fa fa-search" aria-hidden="true"></i> </button>
+                                <a href="{{ route('kategori.index') }}" class="btn btn-warning text-light ml-2"> <i class="fa fa-refresh" aria-hidden="true"></i> </a>
+                            </div>
+                           
                 </div>
-                </form>
-
-        <div class="row" style="overflow: scroll">
-            <div class="col-md-12">
-                <div class="container bg-white p-4"
-                    style="border-radius:3px;box-shadow:rgba(0, 0, 0, 0.03) 0px 4px 8px 0px">
-           
-                    <a href="{{ route('kategori.create') }}" class="btn btn-primary my-1 float-right">
-                     <i class="fa fa-plus"></i> Tambah Kategori</a>
-                    
-                 <table class="table align-items-center table-flush mt-2">
-                        <thead class="thead-light">
-                            <tr>
-                                <th>No</th>
-                                <th>Nama Kategori</th>
-                                <th>Keterangan</th>
-                                <th>Status</th>
-                                <th>Option</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($kategori as $category)
-                                
-                            <tr>
-                            <td scope="row">
-                                {{ $loop->iteration }}
-                            </td>
-                            <td> 
-                                {{$category->nama_kategori}} 
-                            </td>
-                            <td> 
-                                {{$category->keterangan}} 
-                            </td>
-                            <td>
-                                @if($category->status == "ACTIVE")
-                                <span class="badge badge-success">
-                                Aktif
-                                </span>
-                                @else
-                                <span class="badge badge-danger">
-                                Nonaktif
-                                </span>
-                                @endif
-                                </td>
-                                <td>
-                                 <a class="badge badge-info text-white badge-pill" href="{{route('kategori.edit',
-                                    [$category->id])}}"> <i class="fa fa-edit"></i> Edit</a>
-                                <form onsubmit="return confirm('yakin untuk memasukkan ke Trash!')" class="d-inline" action="{{route('kategori.destroy', [$category->id])}}"   method="POST">
-                                    @method('DELETE')
-                                    @csrf
-                                    <button type="submit" value="Delete" class="badge badge-danger badge-pill">
-                                        <i class="fa fa-trash"></i> Trash
-                                    </button>
-                                    </form>
-                                    </td>
-                            </tr>
-                            
-                        </tbody>
-                        @endforeach
-                    </table>
-
-                    <div class="text-center">
-
-                        {{  $kategori->appends(Request::all())->links() }}
-                    </div>
-                        
-                </div>
+              </form>
             </div>
+            <div class="card-body--">
+              
+              <a class="btn btn-primary btn-sm ml-3 mb-3" href="{{ route('kategori.create') }}"> <i class="fa fa-plus" aria-hidden="true"></i> </a>
+              <div class="table-stats order-table ov-h">
+                <table class="table table-hover">
+                  <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama Kategori</th>
+                        <th>Keterangan</th>
+                        <th>Option</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @foreach ($kategori as $category)
+                        
+                    <tr>
+                    <td scope="row">
+                                            {{ $loop->iteration }}
+                    </td>
+                    <td> 
+                        {{$category->nama_kategori}} 
+                    </td>
+                    <td> 
+                        {{$category->keterangan}} 
+                    </td>
+                
+                        <td>
+                         <a class="btn btn-info text-white btn-sm" href="{{route('kategori.edit',
+                            [$category->id])}}"> <i class="fa fa-edit"></i></a>
+                        <form onsubmit="return confirm('yakin untuk memasukkan ke Trash!')" class="d-inline" action="{{route('kategori.destroy', [$category->id])}}"   method="POST">
+                            @method('DELETE')
+                            @csrf
+                            <button type="submit" value="Delete" class="btn btn-danger btn-sm">
+                                <i class="fa fa-trash"></i>
+                            </button>
+                            </form>
+                            </td>
+                    </tr>
+                    
+                </tbody>
+                @endforeach
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
-</div>
-</div>
-</div>
+      </div>
+    </div>
 
+    
 @endsection
+
