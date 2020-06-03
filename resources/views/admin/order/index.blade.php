df26a4fbdd9c99601ab677f9df82b3af8102428b
mhafet17 <do.crazy192@gmail.com>
2020-06-03 23:21:10 +0700

    Integrasi views index order

--- resources/views/admin/order/index.blade.php
+++ resources/views/admin/order/index.blade.php
@@ -1,119 +1,95 @@
-@extends('admin.layouts.main')
+@extends('admin.layouts.default')
 
-@section('title','Admin - Data Kursus')
-
+@section('title','Bimble - Data Order')
 @section('content')
-    
-<div class="main-content">
-    <section class="section">
-        <div class="section-header">
-            <h1>Management Data Order </h1>
-        </div>
-
-        <nav class="breadcrumb ml-4" style="margin-top: -20px">
-            <a class="breadcrumb-item" href="{{ route('manager.home') }}">Home</a>
-        <a class="breadcrumb-item active" href="{{ route('order.index') }}">Order</a>
-        </nav>
-                 
-                  
-        @if(session('status'))
-        @push('scripts')
-        <script>
-            swal({
-            title: "Success",
-            text: "{{session('status')}}",
-            icon: "success",
-            button: false,
-            timer: 1500
-            });
-        </script>
-        @endpush
-         @endif
-
-             
-         
-
-        <div class="row" style="overflow: scroll">
-            <div class="col-md-12">
-                <div class="container bg-white p-4"
-                    style="border-radius:3px;box-shadow:rgba(0, 0, 0, 0.03) 0px 4px 8px 0px">
-                  
-                    <table class="table align-items-center table-flush">
-                        <thead class="thead-light">
-                            <tr>
-                                <th>ID</th>
-                                <th>Nama</th>
-                                <th>Username</th>
-                                <th>Total Tagihan</th>
-                                <th>Status Order</th>
-                                <th>Option</th>
-                            </tr>
-                        </thead>
-                 
-              
-                        <tbody>
-                            @forelse ($order as $row)                
-                            <tr>
-                                <td> {{ $row->id }} </td>
-                                @foreach ($row->pendaftar as $item)
-                                    <td> {{ $item->nama_pendaftar }} </td>
-                                    <td>{{ $item->username }}</td>
-                                @endforeach
-                                <td>@currency($row->total_tagihan).00</td>
-
-                                @if ($row->status_kursus == 'SUCCESS')
-                                <td>
-                                    <span class="badge badge-success  badge-pill"><i class="fas fa-check-circle "></i>
-                                        {{ $row->status_kursus }}</span>
-                                </td>
-                                @elseif($row->status_kursus == 'CANCEL')
-                                <td>
-                                    <span class="badge badge-danger  badge-pill"><i class="fas fa-backspace"></i>
-                                        {{ $row->status_kursus }}</span>
-                                </td>
-                                @elseif($row->status_kursus == 'FAILED')
-                                <td>
-                                    <span class="badge badge-danger  badge-pill"><i class="fas fa-eye-slash    "></i>
-                                        {{ $row->status_kursus }}</span>
-                                </td>
-                                @else
-                                <td>
-                                    <span class="badge badge-warning badge-pill"><i class="fas fa-bullseye"></i>
-                                        {{ $row->status_kursus }}</span>
-                                </td>
-                                @endif
-                                
-                                <td>
-                                    <a class="btn btn-info text-white btn-sm" href="{{route('order.edit',
-                                  [$row->id])}}"> <i class="fa fa-edit"></i> </a>
-                               <a class="btn btn-warning text-white btn-sm" href="{{route('order.show',
-                                  [$row->id])}}"> <i class="fa fa-eye"></i> </a>
-                                          
-                       <form onsubmit="return confirm('yakin untuk memasukkan ke Trash!')" class="d-inline" action="{{route('order.destroy', [$row->id])}}"   method="POST">
-                        @method('DELETE')
-                        @csrf
-                        <button type="submit" value="Delete" class="btn btn-danger btn-sm">
-                            <i class="fa fa-trash"></i> 
-                        </button>
-                    </form>
-                                </td>
-                            </tr>
-                            @empty
-                            @endforelse
-                        </tbody>
-
-                    </table>
-
-                    <div class="text-center">
-
-        
-                    </div>
-                        
-                </div>
+    <div class="orders">
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+            <div class="card-body">
+              <h4 class="box-title">Daftar Order Masuk</h4>
             </div>
+            <div class="card-body--">
+              <div class="table-stats order-table ov-h">
+                <table class="table table-hover">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Nama</th>
+                      <th>Email</th>
+                      <th>Total Order</th>
+                      <th>Status</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @forelse ($items as $item)
+                      <tr>
+                        <td>{{ $item->id }}</td>
+                        @foreach ($item->pendaftar as $user)
+                            
+                        <td>{{ $user->nama_pendaftar }}</td>
+                        <td>{{ $user->email }}</td>
+                        @endforeach
+                        <td>@currency($item->total_tagihan).00</td>
+                        <td>
+                          @if($item->status_kursus == 'PENDING')
+                            <span class="badge badge-info">
+                          @elseif($item->status_kursus == 'SUCCESS')
+                            <span class="badge badge-success">
+                          @elseif($item->status_kursus == 'FAILED')
+                            <span class="badge badge-warning">
+                          @else
+                            <span>
+                          @endif
+                            {{ $item->status_kursus }}
+                            </span>
+                        </td>
+                        <td>
+                          @if($item->status_kursus == 'PENDING')
+                            <a href="{{ route('order.status', $item->id) }}?status=SUCCESS" class="btn btn-success btn-sm">
+                              <i class="fa fa-check"></i>
+                            </a>
+                            <a href="{{ route('order.status', $item->id) }}?status=FAILED" class="btn btn-warning btn-sm">
+                              <i class="fa fa-times"></i>
+                            </a>
+                          @endif
+                          <a href="#mymodal"
+                            data-remote="{{ route('order.show', $item->id) }}"
+                            data-toggle="modal"
+                            data-target="#mymodal"
+                            data-title="Detail Order {{ $user->nama_pendaftar }}"
+                            class="btn btn-info btn-sm">
+                            <i class="fa fa-eye"></i>
+                          </a>
+                         
+                          <form action="{{ route('order.destroy', $item->id) }}" 
+                                method="post" 
+                                class="d-inline">
+                            @csrf
+                            @method('delete')
+                            <button class="btn btn-danger btn-sm">
+                              <i class="fa fa-trash"></i>
+                            </button>
+                          </form>
+                        </td>
+                      </tr>
+                    @empty
+                        <tr>
+                          <td colspan="6" class="text-center p-5">
+                            Data tidak tersedia
+                          </td>
+                        </tr>
+                    @endforelse
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
-</div>
-</div>
-</div>
+      </div>
+    </div>
 
+    
 @endsection
+
