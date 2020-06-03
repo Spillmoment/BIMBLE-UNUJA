65648bb611b58adb7e9cba9518b2a8058c7538f1
mhafet17 <do.crazy192@gmail.com>
2020-06-03 23:17:40 +0700

    Integrasi views edit gallery

--- resources/views/admin/gallery/edit.blade.php
+++ resources/views/admin/gallery/edit.blade.php
@@ -1,25 +1,12 @@
-@extends('admin.layouts.main')
+@extends('admin.layouts.default')
 
-@section('title','Admin - Ubah Data Gallery')
-
+@section('title','Bimble - Edit Data Gallery')
 @section('content')
-
-  <div class="main-content">
-        <div class="container mt-5">
-            <div class="row">
-                <div class="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2">
-
-                    <div class="card card-primary">
-                        <div class="col-md-12 text-center">
-                            <p class="registration-title font-weight-bold display-4 mt-4" style="font-size: 50px;">
-                                Ubah Gallery</p>
-                            <p style="line-height:-30px;margin-top:-20px;">Silahkan ubah data data yang diperlukan
-                                dibawah </p>
-                            <hr>
-                        </div>
-
-                        <div class="card-body">
-                                <div class="col-md-12 bg-white" style="border-radius:3px;box-shadow:rgba(0, 0, 0, 0.03) 0px 4px 8px 0px">
+    <div class="card">
+      <div class="card-header">
+        <strong>Edit Gallery</strong>
+      </div>
+      <div class="card-body card-block">
                                     <form method="post" action="{{route('gallery.update',$gallery->id)}}" enctype="multipart/form-data">
                                       @csrf
                                      @method('PUT')
@@ -44,17 +31,12 @@
                                       </div>
                                         
                                        
-                                        <button type="submit" class="btn btn-block btn-primary">
-                                          <big>Edit  Gallery</big></button>
+                                      <div class="form-group">
+                                        <button class="btn btn-primary btn-block" type="submit">
+                                          Edit Kursus
+                                        </button>
+                                      </div>
+                                    </form>
+                                  </div>
                                 </div>
-
-
-                            </form>
-                        </div>
-                    </div>
-                 
-                </div>
-            </div>
-        </div>
-  </div>
 @endsection
