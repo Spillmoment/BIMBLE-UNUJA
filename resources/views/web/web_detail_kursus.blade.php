1240b9e905ad20732fb9ef240dc90030ac257542
gunawan <deddygunawan98@gmail.com>
2020-06-14 00:45:23 +0700

    menghilangkan input hide kursu pada blade

--- resources/views/web/web_detail_kursus.blade.php
+++ resources/views/web/web_detail_kursus.blade.php
@@ -112,16 +112,16 @@
           
        
           <hr class="my-4">
-          <form id="booking-form" method="post" action="{{ route('order.post') }}" autocomplete="off" class="form">
+          <form id="booking-form" method="post" action="/order/post/{{ $kursus->slug }}" autocomplete="off" class="form">
             @csrf
 
-            @if (Auth::check())
+            {{-- @if (Auth::check())
             <input  type="hidden" name="id_pendaftar" value="{{ Auth::user()->id }}" contextmenu="">
-            @endif
-            
+            @endif            
             <input  type="hidden" name="id_kursus" value="{{ $kursus->id }}">
             <input  type="hidden" name="biaya_kursus" value="{{ $kursus->biaya_kursus }}">
-            <input  type="hidden" name="diskon_kursus" value="{{ ($kursus->diskon_kursus > 0) ? $kursus->diskon_kursus : 0 }}">
+            <input  type="hidden" name="diskon_kursus" value="{{ ($kursus->diskon_kursus > 0) ? $kursus->diskon_kursus : 0 }}"> --}}
+
             <div class="form-group">
               <label for="diskon" class="form-label">Diskon</label>
               <h3>{{ $kursus->diskon_kursus }}%</h3>
