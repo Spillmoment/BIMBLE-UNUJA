940bd9bcb36c74fa3db0c285265e1e0755dae250
mhafet17 <do.crazy192@gmail.com>
2020-06-18 08:03:05 +0700

    create nilai controller

---
+++ app/Http/Controllers/Tutor/NilaiController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Tutor;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Nilai;
+use Illuminate\Support\Facades\Auth;
+
+class NilaiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $id_tutor = Auth::id();
+        $nilai = Nilai::with(['pendaftar', 'tutor', 'kursus'])
+            ->where('id_tutor', $id_tutor)
+            ->orderBy('created_at', 'DESC')
+            ->paginate(12);
+
+        return view('tutor.nilai.index', [
+            'nilai' => $nilai
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('tutor.nilai.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
