1fc2cebb22a2dec9a2730c87cbb2905b99cbc762
mhafet17 <do.crazy192@gmail.com>
2020-06-22 22:58:51 +0700

    menambah route di header

--- resources/views/web/layouts/header-simple.blade.php
+++ resources/views/web/layouts/header-simple.blade.php
@@ -3,9 +3,10 @@
     <nav class="navbar navbar-expand-lg fixed-top shadow navbar-light bg-white">
         <div class="container-fluid">
             <div class="d-flex align-items-center"><a href="{{route('front.index')}}" class="navbar-brand py-1">
-                    <img src="{{asset('assets/frontend/img/logo.png') }}" alt="Directory logo" style="width: 150px;"></a>
+                    <img src="{{asset('assets/frontend/img/logo.png') }}" alt="Directory logo"
+                        style="width: 150px;"></a>
 
-                    
+
                 <form action="#" id="search" class="form-inline d-none d-sm-flex">
                     <div
                         class="input-label-absolute input-label-absolute-left input-reset input-expand ml-lg-2 ml-xl-3">
@@ -19,27 +20,32 @@
                 </form>
 
             </div>
-            <button type="button" data-toggle="collapse" data-target="#navbarCollapse"
-                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation"
-                class="navbar-toggler navbar-toggler-right"><i class="fa fa-bars"></i></button>
+            <button type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse"
+                aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler navbar-toggler-right"><i
+                    class="fa fa-bars"></i></button>
             <!-- Navbar Collapse -->
             <div id="navbarCollapse" class="collapse navbar-collapse">
                 <ul class="navbar-nav ml-auto">
                     @guest
-                    <li class="nav-item"><a href="#" class="nav-link active">Beranda</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link active">Pusat Bantuan</a></li>
+                    <li class="nav-item"><a href="{{ route('front.index') }}" class="nav-link active">Beranda</a></li>
+                    <li class="nav-item"><a href="{{ route('front.kursus') }}" class="nav-link active">List Kursus</a>
+                    </li>
                     @if (Route::has('register'))
-                        <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">Sign in</a></li>
-                        <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">Sign up</a></li>
+                    <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">Sign in</a></li>
+                    <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">Sign up</a></li>
                     @endif
                     @else
-                        <li class="nav-item"><a href="#" class="nav-link active">Beranda</a></li>
-                        <li class="nav-item"><a href="{{ route('user.kursus.success') }}" class="nav-link active">Kursus ku</a></li>
-                        <li class="nav-item"><a href="{{ route('order.view') }}" class="nav-link active">Cart</a></li>
-                        <li class="nav-item"><a href="#" class="nav-link">{{ Auth::user()->nama_pendaftar }}</a></li>
-                        <li class="nav-item"><a href="{{ route('user.logout') }}" class="nav-link badge badge-primary mt-1 text-light">Log Out</a></li>
-                    
-                        
+                    <li class="nav-item"><a href="{{ route('front.index') }}" class="nav-link active">Beranda</a></li>
+                    <li class="nav-item"><a href="{{ route('front.kursus') }}" class="nav-link active">List Kursus</a>
+                    </li>
+                    <li class="nav-item"><a href="{{ route('user.kursus.success') }}" class="nav-link active">Kursus
+                            ku</a></li>
+                    <li class="nav-item"><a href="{{ route('order.view') }}" class="nav-link active">Cart</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">{{ Auth::user()->nama_pendaftar }}</a></li>
+                    <li class="nav-item"><a href="{{ route('user.logout') }}"
+                            class="nav-link badge badge-primary mt-1 text-light">Log Out</a></li>
+
+
                     @endguest
                 </ul>
             </div>
