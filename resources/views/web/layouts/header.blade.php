53d32cd4e7019d5cae1993133c54ae1d45b180df
mhafet17 <do.crazy192@gmail.com>
2020-06-25 23:47:02 +0700

    Memperbaiki halaman header

--- resources/views/web/layouts/header.blade.php
+++ resources/views/web/layouts/header.blade.php
@@ -31,11 +31,11 @@
                     <li class="nav-item"><a href="{{ route('front.kursus') }}" class="nav-link active">List Kursus</a>
                     </li>
 
-                    <li class="nav-item"><a href="#" class="nav-link active">Pusat Bantuan</a></li>
-
                     @if (Route::has('register'))
-                    <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">Sign in</a></li>
-                    <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">Sign up</a></li>
+                    <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">Daftar</a></li>
+                    <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">
+                     Masuk
+                    </a></li>
                     @endif
                     @else
                     <li class="nav-item"><a href="{{ route('front.index') }}" class="nav-link active">Beranda</a></li>
@@ -45,10 +45,10 @@
                             ku</a></li>
                     <li class="nav-item"><a href="{{ route('order.view') }}" class="nav-link active">Cart</a></li>
                     <li class="nav-item"><a href="#" class="nav-link">{{ Auth::user()->nama_pendaftar }}</a></li>
-                    <li class="nav-item"><a href="{{ route('user.logout') }}"
-                            class="nav-link badge badge-primary mt-1 text-light">Log Out</a></li>
-
-
+                    <li class="nav-item">
+                        <a href="{{ route('user.logout') }}" class="btn btn-primary btn-md" style="border-radius: 20px">
+                            Keluar 
+                          </a></li>
                     @endguest
 
                 </ul>
@@ -57,56 +57,4 @@
     </nav>
     <!-- /Navbar -->
 </header>
-<section class="d-flex align-items-center dark-overlay bg-cover header-utama">
-    <div class="container py-6 py-lg-7 text-white overlay-content text-center">
-        <div class="row">
-            <div class="col-xl-10 mx-auto">
-                <h1 class="display-3 font-weight-bold text-shadow">EH - BIMBEL</h1>
-                <p class="text-lg text-shadow">Pilihlah bimbel favoritmu.</p>
-            </div>
-        </div>
-    </div>
-</section>
-<div class="container">
-    <div class="search-bar rounded p-3 p-lg-4 position-relative mt-n5 z-index-20">
 
-        {{-- Search Bimble  --}}
-
-        <form action="{{ route('front.index') }}">
-            <div class="row">
-                <div class="col-lg-4 d-flex align-items-center form-group">
-                    <input type="text" name="keyword" placeholder="Mau cari Bimbel?"
-                        class="form-control border-0 shadow-0" value="{{ Request::get('keyword') }}">
-                </div>
-
-                <div class="col-md-6 col-lg-2 d-flex align-items-center form-group">
-                    <div class="input-label-absolute input-label-absolute-right w-100">
-                        <label for="location" class="label-absolute"><i class="fa fa-crosshairs"></i>
-                            <div class="sr-only">Kota</div>
-                        </label>
-                        <input type="text" name="location" placeholder="Lokasi" class="form-control border-0 shadow-0">
-                    </div>
-                </div>
-
-                <div class="col-md-6 col-lg-3 d-flex align-items-center form-group no-divider">
-                    <form action="{{ route('front.index') }}">
-                        <select id="nama_kategori" name="nama_kategori" data-style="btn-form-control"
-                            class="selectpicker" value="Kategori">
-
-                            @foreach ($kategori as $row)
-                            <option value="{{$row->id}}">{{ $row->nama_kategori }}</option>
-                            @endforeach
-
-
-                        </select>
-
-                </div>
-                <div class="col-lg-2 form-group mb-0">
-                    <button type="submit" class="btn btn-primary btn-block h-100">Cari</button>
-                </div>
-            </div>
-        </form>
-
-
-    </div>
-</div>
