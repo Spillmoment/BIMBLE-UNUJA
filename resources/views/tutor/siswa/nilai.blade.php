e7b1c6230a3d1ce8049ba235c3b14a3f43d6d6f0
mhafet17 <do.crazy192@gmail.com>
2020-07-11 23:26:45 +0700

    Refactor data di nilai siswa

--- resources/views/tutor/siswa/nilai.blade.php
+++ resources/views/tutor/siswa/nilai.blade.php
@@ -7,21 +7,9 @@
         <strong>Tambah Nilai {{ $siswa->nama_siswa }}</strong>
     </div>
     <div class="card-body card-block">
-        <form action="{{ route('siswa.add') }}" method="POST">
+        <form action="{{ route('siswa.add', [$siswa->id]) }}" method="POST">
             @csrf
             @method('PUT')
-
-            @if (Auth::check())
-            <input type="hidden" name="id_tutor" value="{{ Auth::user()->id }}" contextmenu="">
-            @endif
-
-            <input type="hidden" name="nama_siswa" value="{{ $siswa->nama_siswa }}" contextmenu="">
-            <input type="hidden" name="jenis_kelamin" value="{{ $siswa->jenis_kelamin }}" contextmenu="">
-            <input type="hidden" name="alamat" value="{{ $siswa->alamat }}" contextmenu="">
-            <input type="hidden" name="username" value="{{ $siswa->username }}" contextmenu="">
-            <input type="hidden" name="password" value="{{ $siswa->password }}" contextmenu="">
-            <input type="hidden" name="konfirmasi_password" value="{{ $siswa->password }}" contextmenu="">
-            <input type="hidden" name="keterangan" value="{{ $siswa->keterangan }}" contextmenu="">
 
             <div class="col-md-4">
                 <div class="form-group ">
