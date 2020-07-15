fc12a3ee19d8f93169c71b566b694e006882f201
mhafet17 <do.crazy192@gmail.com>
2020-07-15 08:08:42 +0700

    Integrasi breadcrumbs di nilai detail

--- resources/views/tutor/siswa/nilai.blade.php
+++ resources/views/tutor/siswa/nilai.blade.php
@@ -2,32 +2,75 @@
 
 @section('title','Bimble - Tambah Data Siswa')
 @section('content')
-<div class="card">
-    <div class="card-header">
-        <strong>Tambah Nilai {{ $siswa->nama_siswa }}</strong>
+
+<div class="breadcrumbs">
+    <div class="breadcrumbs-inner">
+        <div class="row m-0">
+            <div class="col-sm-4">
+                <div class="page-header float-left">
+                    <div class="page-title">
+                        <h1>Detail Siswa</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-8">
+                <div class="page-header float-right">
+                    <div class="page-title">
+                        <ol class="breadcrumb text-right">
+                            <li><a href="{{ route('siswa.index') }}">Data Siswa</a></li>
+                            <li class="active">Detail Siswa </li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="content">
+
+    <div class="card">
+        <div class="card-header">
+        <strong>Form Nilai <span class="badge badge-primary badge-lg badge-pill" style="font-size: 15px;">
+            {{ $siswa->nama_siswa }}</span></strong>
     </div>
     <div class="card-body card-block">
         <form action="{{ route('siswa.add', [$siswa->id]) }}" method="POST">
             @csrf
             @method('PUT')
-
+            
             <div class="col-md-4">
                 <div class="form-group ">
                     <label for="nilai">Nilai Siswa</label>
                     <input type="number" class="form-control {{ $errors->first('nilai') ? 'is-invalid' : '' }}"
-                        name="nilai" id="nilai" value="{{old('nilai')}}" placeholder="Nilai Siswa">
+                    name="nilai" id="nilai" value="{{old('nilai')}}" placeholder="Nilai">
                     <div class="invalid-feedback">
                         {{$errors->first('nilai')}}
                     </div>
                 </div>
+                <div class="form-group">
+                   <label for="keterangan">Keterangan</label>
+                   <input name="keterangan" class="form-control {{ $errors->first('keterangan') ? 'is-invalid' : '' }}"
+                       id="keterangan" rows="3" placeholder="Keterangan">{{old('keterangan')}}
+                   <div class="invalid-feedback">
+                       {{$errors->first('keterangan')}}
+                   </div>
+               </div>
             </div>
 
             <div class="form-group">
+                @if ($siswa->nilai != null)
+                <button class="btn btn-primary btn-block" type="submit">
+                    Update Nilai
+                </button>
+                @else
                 <button class="btn btn-primary btn-block" type="submit">
                     Tambah Nilai
                 </button>
+                @endif
             </div>
         </form>
     </div>
+</div>
 </div>
 @endsection
