e12061dc359bb7fd366dc6d9967844a77c98f4b0
mhafet17 <do.crazy192@gmail.com>
2020-07-18 09:01:31 +0700

    Menambah navigasi pengaturan di navbar-tutor

--- resources/views/admin/includes/navbar-tutor.blade.php
+++ resources/views/admin/includes/navbar-tutor.blade.php
@@ -18,12 +18,13 @@
                 <a href="#" class="dropdown-toggle active" data-toggle="dropdown" aria-haspopup="true"
                     aria-expanded="false">
                     <img class="user-avatar rounded-circle img-thumbnail img-fluid"
-                        src="{{ url('uploads/tutor/'.Auth::user()->foto ) }}" alt="User Avatar" height="70px"
+                        src="{{ Storage::url('public/'.Auth::user()->foto ) }}" alt="User Avatar" height="70px"
                         width="70px">
                 </a>
 
                 <div class="user-menu dropdown-menu">
                     <a class="nav-link" href="#"><i class="fa fa-user"></i>{{ Auth::user()->nama_tutor }}</a>
+                    <a class="nav-link" href="{{ route('tutor.profile') }}"><i class="fa fa-cog"></i>Settings</a>
                     <a class="nav-link" href="{{ route('tutor.logout') }}"><i class="fa fa-power-off"></i>Logout</a>
                 </div>
 
