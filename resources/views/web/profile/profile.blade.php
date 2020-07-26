46d775fc191bd4fe55e8c009b37aee019d2c1bf2
mhafet17 <do.crazy192@gmail.com>
2020-07-26 08:55:31 +0700

    Memambah fungsi readonly di form profile

--- resources/views/web/profile/profile.blade.php
+++ resources/views/web/profile/profile.blade.php
@@ -2,7 +2,7 @@
 
     @if (session('success'))
     <div class="alert alert-success alert-dismissible fade show" role="alert">
-        <strong>{{ session('success') }}</strong>
+        Data <strong>{{ session('success') }}</strong> Berhasil Diupdate!
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
@@ -27,7 +27,7 @@
                     <label for="nama_lengkap" class="form-label"> Nama Lengkap </label>
                     <input name="nama_pendaftar" id="nama_lengkap" type="text" placeholder="Masukan Nama Lengkap"
                         class="form-control {{ $errors->first('nama_pendaftar') ? 'is-invalid' : '' }}"
-                        value="{{old('nama_pendaftar', Auth::user()->nama_pendaftar) }}">
+                        readonly value="{{old('nama_pendaftar', Auth::user()->nama_pendaftar) }}">
                     <div class="invalid-feedback">
                         {{$errors->first('nama_pendaftar')}}
                     </div>
@@ -37,7 +37,7 @@
                     <label for="email" class="form-label"> Email</label>
                     <input name="email" id="email" type="email" placeholder="Masukkan Email"
                         class="form-control {{ $errors->first('email') ? 'is-invalid' : '' }}"
-                        value="{{old('email',  Auth::user()->email) }}">
+                        readonly value="{{old('email',  Auth::user()->email) }}">
                     <div class="invalid-feedback">
                         {{$errors->first('email')}}
                     </div>
@@ -49,15 +49,15 @@
                 <div class="col form-group">
                     <label for="jenis_kelamin" class="form-label"> Jenis Kelamin</label>
                     <div class="form-check {{ $errors->first('jenis_kelamin') ? 'is-invalid' : '' }}"
-                        value="{{old('jenis_kelamin') }}">
+                        readonly value="{{old('jenis_kelamin') }}">
                         <div class="custom-control custom-radio">
                             <input type="radio" id="laki-laki" name="jenis_kelamin" class="custom-control-input "
-                                value="L" required {{ Auth::user()->jenis_kelamin == 'L' ? 'checked' : ''}}>
+                                readonly value="L" required {{ Auth::user()->jenis_kelamin == 'L' ? 'checked' : ''}}>
                             <label for="laki-laki" class="custom-control-label">Laki-laki</label>
                         </div>
                         <div class="custom-control custom-radio">
                             <input type="radio" id="perempuan" name="jenis_kelamin" class="custom-control-input"
-                                value="P" required {{ Auth::user()->jenis_kelamin == 'P' ? 'checked' : ''}}>
+                                readonly value="P" required {{ Auth::user()->jenis_kelamin == 'P' ? 'checked' : ''}}>
 
                             <label for="perempuan" class="custom-control-label">Perempuan</label>
                         </div>
@@ -69,7 +69,7 @@
                     <label for="username" class="form-label"> Username</label>
                     <input name="username" id="username" type="text" placeholder="Masukkan Username"
                         class="form-control {{ $errors->first('username') ? 'is-invalid' : '' }}"
-                        value="{{old('username',  Auth::user()->username) }}">
+                        readonly value="{{old('username',  Auth::user()->username) }}">
                     <div class="invalid-feedback">
                         {{$errors->first('username')}}
                     </div>
@@ -80,10 +80,10 @@
 
                 <div class="col form-group">
                     <label for="foto" class="form-label">Foto</label>
-                    <input type="file" class="form-control-file {{ $errors->first('foto') ? 'is-invalid' : '' }}"
+                    <input readonly type="file" class="form-control-file {{ $errors->first('foto') ? 'is-invalid' : '' }}"
                         name="foto" id="foto">
                     <div class="mt-3">
-                        <img class="img-target" width="200px">
+                        <img id="img" class="img-target" width="200px">
                     </div>
                     <div class="invalid-feedback">
                         {{$errors->first('foto')}}
@@ -92,7 +92,7 @@
 
                 <div class="col form-group">
                     <label for="alamat" class="form-label">Alamat</label>
-                    <textarea rows="4" name="alamat" id="alamat" placeholder="Masukkan Alamat"
+                    <textarea readonly rows="4" name="alamat" id="alamat" placeholder="Masukkan Alamat"
                         class="form-control {{ $errors->first('alamat') ? 'is-invalid' : '' }}">
                                          {{old('alamat',Auth::user()->alamat) }}
                     </textarea>
@@ -103,7 +103,13 @@
                 </div>
 
             </div>
-            <button class="btn btn-primary btn-block" type="submit">Simpan</button>
+           
+                <div class="float-right my-3">
+                    <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i>&nbsp;&nbsp;Simpan</button>
+                    <button type="button" class="btn btn-success" id="ubah"><i class="fa fa-pen"></i>&nbsp;&nbsp;Ubah</button>
+                    <button type="reset" class="btn btn-danger"><i class="fa fa-times"></i>&nbsp;&nbsp;Batal</button>
+                </div>
+                
         </form>
     </div> <!-- card-body.// -->
 
@@ -112,6 +118,7 @@
 @push('scripts')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script>
+
     $(document).ready(function () {
 
         var readURL = function (input) {
@@ -128,6 +135,22 @@
         $(".form-control-file").on('change', function () {
             readURL(this);
         });
+
+        $('#ubah').on('click', function() {
+				$('#nama_lengkap').prop('readonly', false)
+				$('#email').prop('readonly', false)
+				$('#username').prop('readonly', false)
+				$('#alamat').prop('readonly', false)
+			});
+
+			$('button[type="reset"]').on('click', function() {
+				$('#nama_lengkap').prop('readonly', true)
+				$('#email').prop('readonly', true)
+				$('#username').prop('readonly', true)
+				$('#alamat').prop('readonly', true)
+				$('#foto').prop('readonly', true)
+			});
+
     });
 
 </script>
