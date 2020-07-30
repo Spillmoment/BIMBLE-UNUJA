0915e76e54f6ceca02cd8d00aa8c19ad8179dc0e
mhafet17 <do.crazy192@gmail.com>
2020-07-30 20:27:51 +0700

    Menambah view pengaturan tutor

--- resources/views/tutor/pengaturan/index.blade.php
+++ resources/views/tutor/pengaturan/index.blade.php
@@ -45,107 +45,45 @@
 
 <div class="content">
   
-  @foreach ($tutor as $tutor)
+
   <div class="card">
       <div class="card-header">
           <strong>Pengaturan
-              <span class="badge badge-pill badge-primary"> {{ $tutor->nama_tutor }} </span>
+              <span class="badge badge-pill badge-primary"> {{ Auth::user()->nama_tutor }} </span>
           </strong>
       </div>
       <div class="card-body card-block">
-          <form method="post" enctype="multipart/form-data" action="{{route('tutor.update-profil',[$tutor->id])}}">
+          <form method="post" enctype="multipart/form-data" action="{{route('tutor.update-pengaturan', Auth::id() )}}">
               @csrf
               @method('PUT')
 
-              <div class="form-group ">
-                  <label for="nama_tutor">Nama Tutor</label>
-                  <input type="text" class="form-control {{ $errors->first('nama_tutor') ? 'is-invalid' : '' }}"
-                      name="nama_tutor" id="nama_tutor" value="{{ $tutor->nama_tutor }}" placeholder="Nama tutor">
-                  <div class="invalid-feedback">
-                      {{$errors->first('nama_tutor')}}
-                  </div>
-              </div>
+              <div class="col form-group ">
+                <label for="curr_password">Current Password</label>
+                <input type="password" class="form-control {{ $errors->first('current_password') ? 'is-invalid' : '' }}"
+                    name="current_password" id="curr_password" value="" placeholder="Current Password">
+                <div class="invalid-feedback">
+                    {{$errors->first('current_password')}}
+                </div>
+            </div>
 
-              <div class="form-group">
-                  <label for="alamat">Alamat</label>
-                  <textarea name="alamat" class="form-control {{ $errors->first('alamat') ? 'is-invalid' : '' }}"
-                      id="alamat" rows="3" placeholder="Alamat">{{ $tutor->alamat}}</textarea>
-                  <div class="invalid-feedback">
-                      {{$errors->first('alamat')}}
-                  </div>
-              </div>
+            <div class="col form-group ">
+                <label for="password">Password</label>
+                <input type="password" class="form-control {{ $errors->first('password') ? 'is-invalid' : '' }}"
+                    name="password" id="password" value="" placeholder="Password">
+                <div class="invalid-feedback">
+                    {{$errors->first('password')}}
+                </div>
+            </div>
 
-              <div class="form-group ">
-                  <label for="email">Email</label>
-                  <input type="email" class="form-control {{ $errors->first('email') ? 'is-invalid' : '' }}"
-                      name="email" id="email" value="{{$tutor->email}}" placeholder="Email">
-                  <div class="invalid-feedback">
-                      {{$errors->first('email')}}
-                  </div>
-              </div>
-
-              <div class="row">
-                <div class="col-4">
-                  <div class="form-group">
-                      <label for="foto">Foto</label>
-                      <input type="file" class="form-control-file {{ $errors->first('foto') ? 'is-invalid' : '' }}"
-                      name="foto" id="foto">
-                      <small class="text-muted">Kosongkan jika tidak ingin mengubah
-                        foto</small>
-                        <div class="invalid-feedback">
-                            {{$errors->first('foto')}}
-                        </div>
-                      </div> 
-                      
-                    </div>
-                    <div class="col-4">
-
-                      <div class="form-group">
-                        <img src="{{Storage::url('public/' . $tutor->foto)}}" width="96px" />
-                      </div>
-                      
-                    </div>
-              </div>
-
-
-              <div class="form-group ">
-                  <label for="username">Username</label>
-                  <input type="username" class="form-control {{ $errors->first('username') ? 'is-invalid' : '' }}"
-                      name="username" id="username" value="{{ $tutor->username }}" placeholder="Username">
-                  <div class="invalid-feedback">
-                      {{$errors->first('username')}}
-                  </div>
-              </div>
-
-
-              <div class="form-group ">
-                  <label for="password">Password</label>
-                  <input type="password" class="form-control {{ $errors->first('password') ? 'is-invalid' : '' }}"
-                      name="password" id="password" value="" placeholder="Password">
-                  <div class="invalid-feedback">
-                      {{$errors->first('password')}}
-                  </div>
-              </div>
-
-              <div class="form-group ">
-                  <label for="password">Konfirmasi Password</label>
-                  <input type="password" class="form-control {{ $errors->first('konfirmasi_password') ? 'is-invalid' : '' }}"
-                      name="konfirmasi_password" id="password" value=""
-                      placeholder="Password">
-                  <div class="invalid-feedback">
-                      {{$errors->first('konfirmasi_password')}}
-                  </div>
-              </div>
-
-
-              <div class="form-group ">
-                  <label for="keahlian">Keahlian</label>
-                  <input type="keahlian" class="form-control {{ $errors->first('keahlian') ? 'is-invalid' : '' }}"
-                      name="keahlian" id="keahlian" value="{{ $tutor->keahlian }}" placeholder="Keahlian">
-                  <div class="invalid-feedback">
-                      {{$errors->first('keahlian')}}
-                  </div>
-              </div>  
+            <div class="col form-group ">
+                <label for="password">Konfirmasi Password</label>
+                <input type="password"
+                    class="form-control {{ $errors->first('konfirmasi_password') ? 'is-invalid' : '' }}"
+                    name="konfirmasi_password" id="password" value="" placeholder="Konfirmasi Password">
+                <div class="invalid-feedback">
+                    {{$errors->first('konfirmasi_password')}}
+                </div>
+            </div>
               
               <div class="form-group">
                 <button type="submit" class="btn btn-primary btn-block">Simpan</button>
@@ -154,7 +92,6 @@
           </form>
       </div>
   </div>
-  @endforeach
 </div>
 @endsection
 
